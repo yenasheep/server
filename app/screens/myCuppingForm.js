de6f67c051054e459e783790585ad15baee0183d
0xbc614e <0xbc614e@gmail.com>
2020-11-01 20:40:31 +0900

    Make DB retrieval as function

--- app/screens/myCuppingForm.js
+++ app/screens/myCuppingForm.js
@@ -6,6 +6,10 @@ import { createStackNavigator } from '@react-navigation/stack';
 import createCuppingForm from "../screens/createCuppingForm";
 import createForm from '../screens/createCuppingForm';
 
+// 안드로이드 에뮬레이터는 저 IP 써야 내 컴퓨터 localhost랑 통신 가능.
+// URL 저걸 localhost로 하면 에뮬레이터 자체 localhost를 의미하는 거라 내 컴퓨터 localhost에 닿질 않음.
+const DATABASE_URL = "http://10.0.2.2:2020/db";
+
 export default class MyCuppingForm extends Component{
   constructor(props) {
     super(props)
@@ -15,13 +19,14 @@ export default class MyCuppingForm extends Component{
     };
   }
 
-  async componentDidMount() {
-    // 안드로이드 에뮬레이터는 저 IP 써야 내 컴퓨터 localhost랑 통신 가능.
-    // URL 저걸 localhost로 하면 에뮬레이터 자체 localhost를 의미하는 거라 내 컴퓨터 localhost에 닿질 않음.
-    const url = "http://10.0.2.2:2020/db";
-    const res = await fetch(url);
+  async refreshList() {
+    const res = await fetch(DATABASE_URL);
     const result = await res.json();
     this.setState({forms: result});
+  }
+
+  componentDidMount() {
+    this.refreshList();
   }
 
   render(){
