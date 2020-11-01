d34dd5880fb5dcd57d0fa51af0fa99fedeca0ed1
JKTG94 <55914016+JKTG94@users.noreply.github.com>
2020-11-02 08:14:16 +0900

    임시
    imagepicker가 구현되지 않음

--- app/screens/CreateFeed.js
+++ app/screens/CreateFeed.js
@@ -2,18 +2,12 @@ import React, { Component } from 'react';
 import { View, Image, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Card, Fab, Form, CardItem, Textarea, Footer, Thumbnail, Body, Left, Right, Button, Icon, Container, Content } from 'native-base'
 import { Camera } from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
+import Constants from 'expo-constants';
+import { FontAwesome } from '@expo/vector-icons';
+import CameraRoll from './CameraRoll';
 
-export default class CreateFeed extends Component{
-    state = {
-        hasPermission: null,
-    }
-
-    componentDidMount = async () => {
-        const { status } = await Permissions.askAsync(Permissions.CAMERA)
-      
-        console.log(status)
-    }
-    
+export default class CreateFeed extends Component{    
     render(){
         return(
             <Container>
@@ -25,6 +19,16 @@ export default class CreateFeed extends Component{
                     <Form>
                         <Textarea rowSpan={15} bordered placeholder="내용" 
                         onChangeText= {(text) => this.setState({listText: text})}/>
+                    </Form>
+                    <Form>
+                        <Button transparent>
+                            <FontAwesome name="photo" 
+                            size={24} 
+                            color="green"
+                            style= {styles.photo}
+                            onpress ={CameraRoll}
+                            />
+                        </Button>
                     </Form>
                 </Content>
                 <Footer>
@@ -38,3 +42,14 @@ export default class CreateFeed extends Component{
         );
     }
 }
+
+const styles= StyleSheet.create(
+    {
+        photo:{
+            marginLeft : 10
+        }
+    }
+)
+
+//onPress={pickImage}
+//{image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
