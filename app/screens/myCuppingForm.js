efcbf33f1c5e7986de9a68fb48124fd87c5cdd86
0xbc614e <0xbc614e@gmail.com>
2020-11-02 01:45:48 +0900

    Remove wrong comment

--- app/screens/myCuppingForm.js
+++ app/screens/myCuppingForm.js
@@ -6,8 +6,6 @@ import { createStackNavigator } from '@react-navigation/stack';
 import createCuppingForm from "../screens/createCuppingForm";
 import createForm from '../screens/createCuppingForm';
 
-// 안드로이드 에뮬레이터는 저 IP 써야 내 컴퓨터 localhost랑 통신 가능.
-// URL 저걸 localhost로 하면 에뮬레이터 자체 localhost를 의미하는 거라 내 컴퓨터 localhost에 닿질 않음.
 import { DATABASE_URL } from "../../conf";
 
 export default class MyCuppingForm extends Component{
