6458427155ac025fc9f4d11ca5f57c1193b10ab5
0xbc614e <0xbc614e@gmail.com>
2020-11-01 21:24:51 +0900

    Separate database URL

--- app/screens/myCuppingForm.js
+++ app/screens/myCuppingForm.js
@@ -8,7 +8,7 @@ import createForm from '../screens/createCuppingForm';
 
 // 안드로이드 에뮬레이터는 저 IP 써야 내 컴퓨터 localhost랑 통신 가능.
 // URL 저걸 localhost로 하면 에뮬레이터 자체 localhost를 의미하는 거라 내 컴퓨터 localhost에 닿질 않음.
-const DATABASE_URL = "http://10.0.2.2:2020/db";
+import { DATABASE_URL } from "../../conf";
 
 export default class MyCuppingForm extends Component{
   constructor(props) {
