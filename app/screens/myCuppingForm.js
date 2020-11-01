22d2ca2bfadaa8e88cc3bd96d1d71a412d36e79a
0xbc614e <0xbc614e@gmail.com>
2020-11-01 20:23:49 +0900

    Clarify why 10.0.2.2

--- app/screens/myCuppingForm.js
+++ app/screens/myCuppingForm.js
@@ -16,6 +16,8 @@ export default class MyCuppingForm extends Component{
   }
 
   async componentDidMount() {
+    // 안드로이드 에뮬레이터는 저 IP 써야 내 컴퓨터 localhost랑 통신 가능.
+    // URL 저걸 localhost로 하면 에뮬레이터 자체 localhost를 의미하는 거라 내 컴퓨터 localhost에 닿질 않음.
     const url = "http://10.0.2.2:2020/db";
     const res = await fetch(url);
     const result = await res.json();
